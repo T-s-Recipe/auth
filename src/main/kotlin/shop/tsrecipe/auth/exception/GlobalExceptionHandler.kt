4105c51f8d86e2e58dcb81c5f958bc7a7ae3d9d2
jonghyukleee <jh5253krkr@naver.com>
2025-07-29 18:37:34 +0900

    add: context-path

--- src/main/kotlin/shop/tsrecipe/auth/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/shop/tsrecipe/auth/exception/GlobalExceptionHandler.kt
@@ -2,7 +2,6 @@ package shop.tsrecipe.auth.exception
 
 import com.auth0.jwt.exceptions.JWTVerificationException
 import com.auth0.jwt.exceptions.TokenExpiredException
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -11,18 +10,6 @@ import shop.tsrecipe.auth.util.baseResponse
 
 @RestControllerAdvice
 class GlobalExceptionHandler: Logging {
-    @ExceptionHandler(Exception::class)
-    fun handleUndefinedException(e: Exception): ResponseEntity<ErrorResponse> {
-        logger.error { """
-            Undefined exception.
-            $e
-        """.trimIndent() }
-        return baseResponse(
-            status = HttpStatus.INTERNAL_SERVER_ERROR,
-            body = ErrorResponse(ErrorCode.UNDEFINED_EXCEPTION)
-        )
-    }
-
     @ExceptionHandler(BaseException::class)
     fun handleBaseException(e: BaseException): ResponseEntity<ErrorResponse> {
         return baseResponse(e.httpStatus, ErrorResponse(e))
