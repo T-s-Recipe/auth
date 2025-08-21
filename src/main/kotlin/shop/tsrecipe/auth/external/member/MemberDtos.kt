a5ac67f8e6146754d6f4dd303eb89d72f695717a
jonghyukleee <jh5253krkr@naver.com>
2025-08-21 14:53:55 +0900

    modify: `MemberResponse`
    - Member 수정에 따른 응답 DTO 변경

--- src/main/kotlin/shop/tsrecipe/auth/external/member/MemberDtos.kt
+++ src/main/kotlin/shop/tsrecipe/auth/external/member/MemberDtos.kt
@@ -6,7 +6,5 @@ data class MemberResponse(
     val id: String,
     val oauthProvider: OAuthProvider,
     val oauthId: String,
-    val name: String,
-    val email: String,
     val nickname: String
 )
